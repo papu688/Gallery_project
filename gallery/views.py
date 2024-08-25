0115b44bacc9c86e96876870388fba065d5a4840
papu_68 <giorgobianipaul@gmail.com>
2024-08-25 16:41:03 +0200

    add viewsets for Artist, Artwork and Exhibition models

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Artist, Artwork, Exhibition
+from .serializers import ArtistSerializer, ArtworkSerializer, ExhibitionSerializer
 
-# Create your views here.
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class ArtworkViewSet(viewsets.ModelViewSet):
+    queryset = Artwork.objects.all()
+    serializer_class = ArtworkSerializer
+
+class ExhibitionViewSet(viewsets.ModelViewSet):
+    queryset = Exhibition.objects.all()
+    serializer_class = ExhibitionSerializer
