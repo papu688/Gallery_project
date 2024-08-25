0053979ad8ec846def3ca9f86d41f127a8096938
papu_68 <giorgobianipaul@gmail.com>
2024-08-25 17:00:50 +0200

    add IsAdminOrReadOnly custom permission class

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ArtistViewSet, ArtworkViewSet, ExhibitionsViewSet 
+from .views import ArtistViewSet, ArtworkViewSet, ExhibitionViewSet 
 
 
 router = DefaultRouter()
 
 router.register(r'artists', ArtistViewSet)
-router.register(r'artworls', ArtworkViewSet)
-router.register(r'exhibitions', ExhibitionsViewSet)
+router.register(r'artworks', ArtworkViewSet)
+router.register(r'exhibitions', ExhibitionViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Artist, Artwork, Exhibition
 from .serializers import ArtistSerializer, ArtworkSerializer, ExhibitionSerializer
+from .permissions import IsAdminOrReadOnly
+
 
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
@@ -9,6 +11,7 @@ class ArtistViewSet(viewsets.ModelViewSet):
 class ArtworkViewSet(viewsets.ModelViewSet):
     queryset = Artwork.objects.all()
     serializer_class = ArtworkSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class ExhibitionViewSet(viewsets.ModelViewSet):
     queryset = Exhibition.objects.all()
